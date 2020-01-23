e3971119c64001a55f50906159b2c90093bfa870
Ivan5 <converse_ivan@hotmail.com>
2020-01-23 16:46:23 -0600

    Finish Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Icon } from "react-icons-kit";
+import { iosWorld } from "react-icons-kit/ionicons/iosWorld";
+import { arrowSortedDown } from "react-icons-kit/typicons/arrowSortedDown";
 
 class Footer extends Component {
+  state = {
+    langContent: false
+  };
+
+  handleToggle = e => {
+    e.preventDefault();
+    this.setState({
+      langContent: !this.state.langContent
+    });
+  };
   render() {
     return (
       <FooterContainer>
@@ -69,7 +82,22 @@ class Footer extends Component {
               <Link>Coorporate</Link>
             </li>
           </ul>
+          <div className="lang-btn" onClick={this.handleToggle}>
+            <Icon icon={iosWorld} size={20} />
+            &nbsp;&nbsp;English&nbsp;&nbsp;
+            <Icon icon={arrowSortedDown} />
+          </div>
         </div>
+        {this.state.langContent && (
+          <div className="lang-toggle">
+            <ul>
+              <li>English</li>
+            </ul>
+            <ul>
+              <li>French</li>
+            </ul>
+          </div>
+        )}
       </FooterContainer>
     );
   }
@@ -109,5 +137,34 @@ const FooterContainer = styled.footer`
   span {
     margin-left: 15%;
     font-size: 1.125rem;
+  }
+
+  .lang-btn {
+    background: transparent;
+    border: 0.9px solid #333;
+    padding: 1rem;
+    width: 8rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin: 2rem 0 2rem;
+    cursor: pointer;
+  }
+
+  .lang-toggle {
+    margin-left: 15%;
+    position: absolute;
+    margin-top: -2rem;
+  }
+
+  .lang-toggle ul {
+    background: var(--main-deep-dark);
+    width: 8.125rem;
+    border: 1px solid #333;
+    text-align: center;
+
+    &:hover {
+      background: #0085ff;
+      color: #fff;
+    }
   }
 `;
