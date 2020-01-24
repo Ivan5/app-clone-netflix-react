0fe3332860dfbdd4a4d8706bbbf560a06bda2432
Ivan5 <converse_ivan@hotmail.com>
2020-01-24 11:12:59 -0600

    Make a Login component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./css/App.css";
 import Main from "./pages";
 import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Main} />
+      <Route path="/" component={Main} exact />
+      <Route path="/login" component={Login} />
     </Switch>
   );
 }
