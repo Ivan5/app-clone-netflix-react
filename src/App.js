33e69e47a5f1b3800c60a6527a00da0d43c9c6ad
Ivan5 <converse_ivan@hotmail.com>
2020-01-27 15:30:00 -0600

    Make a ChoosePlan component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./css/App.css";
 import Main from "./pages";
 import { Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import ChoosePlan from "./pages/ChoosePlan";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Main} exact />
       <Route path="/login" component={Login} />
+      <Route path="/choose-plan" component={ChoosePlan} />
     </Switch>
   );
 }
